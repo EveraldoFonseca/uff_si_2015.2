0edef3fcafeb1941e3ad5edb991974eb4d9a28cf
Elbe Miranda <elbemiranda@gmail.com>
2016-01-18 02:32:55 -0200

    Criação das pastas de Debug

---
+++ Trabalho 1 - ED II/auxiliar.c
@@ -0,0 +1,11 @@
+#include "auxiliar.h"
+
+void *initIntElem(int elem) {
+    int* r = (int*) malloc(sizeof (int));
+    *r = elem;
+    return (void*) r;
+}
+void visitIntElem(void *elem) {
+    int* p = (int*) elem;
+    printf("%d\n", p);
+}
