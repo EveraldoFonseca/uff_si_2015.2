0ff58aad14a12bc569381f68be5c71801505c185
LII <lii@lab.local>
2016-01-18 20:20:27 -0200

    Árvore OK

--- Trabalho 1 - ED II/binary_tree.c
+++ Trabalho 1 - ED II/binary_tree.c
@@ -1,48 +1,64 @@
+#include <stdlib.h>
 #include "binary_tree.h"
 #include "auxiliar.h"
 #include "stack.h"
+#include "queue.h"
 
+
+void postOrderTraversalNode( struct BinaryTreeNode *node);
+void inOrderTraversalNode(struct BinaryTreeNode *node);
 void insertBinaryTree(struct BinaryTree *tree, void *elem)
 {
-    struct BinaryTreeNode *novoNo = (struct BinaryTreeNode*) malloc(sizeof(struct BinaryTreeNode));
+    struct BinaryTreeNode* novoNo = (struct BinaryTreeNode*) malloc(sizeof (struct BinaryTreeNode));
     novoNo->elem = elem;
     novoNo->left = NULL;
-    novoNo->right=NULL;
+    novoNo->right = NULL;
 
-    if (tree->root==NULL)
+    struct BinaryTreeNode* auxNo;
+
+    if (tree->root == NULL)
     {
         tree->root = novoNo;
     }
     else
     {
-        struct BinaryTreeNode *auxNo;
         auxNo = tree->root;
-        int side = 0;
-        do
+        while (auxNo != NULL)
         {
-            if (auxNo->left == NULL)
+            int elemNovoNo = *(int*)elem;
+            int elemRaiz = *(int*)auxNo->elem;
+            if (elemNovoNo <= elemRaiz )
             {
-                auxNo->left = novoNo;
-                break;
-            }
-            else if (auxNo->right == NULL)
-            {
-                auxNo->right = novoNo;
-                break;
+                if (auxNo->left == NULL)
+                {
+                    auxNo->left = novoNo;
+                    break;
+                }
+                else
+                {
+                    auxNo = auxNo->left;
+                }
             }
             else
             {
-                auxNo = side ? auxNo->left : auxNo->right;
-                side = !side;
+                if (auxNo->right == NULL)
+                {
+                    auxNo->right = novoNo;
+                    break;
+                }
+                else
+                {
+                    auxNo = auxNo->right;
+                }
             }
         }
-        while (auxNo != NULL);
     }
+
 }
 
 void preOrderTraversal(struct BinaryTree *tree)
 {
-    if (tree->root != NULL)
+    if ((tree != NULL) && (tree->root != NULL))
     {
         struct Stack *pilha = (struct Stack*) malloc(sizeof(struct Stack));
         initStack(pilha);
@@ -62,14 +78,42 @@ void preOrderTraversal(struct BinaryTree *tree)
 }
 void postOrderTraversal(struct BinaryTree *tree)
 {
-
+    if ((tree != NULL) && (tree->root != NULL))
+        postOrderTraversalNode(tree->root);
 }
-
+void postOrderTraversalNode( struct BinaryTreeNode *node)
+{
+    if (node->left != NULL) postOrderTraversalNode(node->left);
+    if (node->right != NULL) postOrderTraversalNode(node->right);
+    visitIntElem(node->elem);
+}
 void inOrderTraversal(struct BinaryTree *tree)
 {
+    if ((tree != NULL) && (tree->root != NULL))
+        inOrderTraversalNode(tree->root);
+}
+void inOrderTraversalNode(struct BinaryTreeNode *node)
+{
+    if (node->left != NULL) inOrderTraversalNode(node->left);
+    visitIntElem(node->elem);
+    if (node->right != NULL) inOrderTraversalNode(node->right);
 
 }
 void breadthFirstTraversal(struct BinaryTree *tree)
 {
-
+   if (!tree->root)return;
+    struct Queue *fila = (struct Queue*) malloc(sizeof (struct Queue));
+    initQueue(fila);
+    struct BinaryTreeNode *node = (struct BinaryTreeNode*) malloc(sizeof( struct BinaryTreeNode));
+    enqueue(fila, tree->root);
+    while (!isEmptyQueue(fila)) {
+        node = dequeue(fila);
+        visitIntElem(node->elem);
+        if (node->left)
+            enqueue(fila, node->left);
+        if (node->right)
+            enqueue(fila, node->right);
+    }
+    clearQueue(fila);
+    free(fila);
 }
